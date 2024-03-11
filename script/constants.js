879c82712a0acc01cc4548afe6db06e7bd62e2eb
Samvel8 <samveljamalyan@gmail.com>
2024-03-11 22:08:59 +0400

    create header

---
+++ script/constants.js
@@ -0,0 +1,7 @@
+export const text = {
+    hy: {
+        warningText: 'Թեստավորմանը մասնակցելու համար խնդրում ենք մուտքագրել գաղտնաբառը'
+    }
+}
+
+export const defaultLanguage = 'hy';
---
+++ script/index.js
@@ -0,0 +1,8 @@
+import { text, defaultLanguage } from "./constants.js";
+
+const warningElement = document.getElementById('warning');
+const warningText = text['hy'].warningText;
+
+for (let i = 0; i < warningText.length; i++) {
+    
+}
